dc0df722ff0f543b3b259b1b748aa1fc94c0d942
lms <lms19880914@qq.com>
2024-12-31 16:04:33 +0800

    增加出版物模块

--- src/main/java/net/csibio/labic/controller/PublicationController.java
+++ src/main/java/net/csibio/labic/controller/PublicationController.java
@@ -2,14 +2,9 @@ package net.csibio.labic.controller;
 
 import net.csibio.labic.domain.Result;
 import net.csibio.labic.domain.db.Publication;
-import net.csibio.labic.domain.db.User;
 import net.csibio.labic.domain.query.UserQuery;
 import net.csibio.labic.repository.PublicationRepository;
-import net.csibio.labic.repository.UserRepository;
 import net.csibio.labic.utils.PublicationUtil;
-import org.jbibtex.BibTeXEntry;
-import org.jbibtex.BibTeXObject;
-import org.jbibtex.BibTeXParser;
 import org.jbibtex.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -20,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.StringReader;
-import java.util.Collection;
 import java.util.List;
 
 @RestController
